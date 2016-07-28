ef42a7534e4d0649a9f0aad6ddad8ba366ac1efd
Aleksey Dolgiy <AVDolgiy.SBT@sberbank.ru>
2016-07-28 15:34:18 +0300

    task2 gen-class

--- task02-new/src/task02/Database.clj
+++ task02-new/src/task02/Database.clj
@@ -7,6 +7,12 @@
   ;;       (task02.query/perform-query) и возвращать результат в виде строки в формате EDN.
 
   ;; Hint: load-initial-data, pr-str, perform-query
-  ; :implement-me
+  (:gen-class
+    :methods [^:static [InitDatabase [] void]
+              ^:static [Select [String] String]]))
+
+(defn -InitDatabase [this]
   )
 
+(defn -Select [this query]
+  )
